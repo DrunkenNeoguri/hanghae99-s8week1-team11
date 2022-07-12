9321d202471a1fc439bb0daf77e002780adbfee0
developneoguri <developneoguri@gmail.com>
2022-07-12 17:05:16 +0900

    메인 화면 슬라이드쇼 구현 완료

---
+++ static/js/main.js
@@ -0,0 +1,44 @@
+let slideIdx = 0;
+const prevbtn = document.querySelector('.container--image__prevbtn');
+const nextbtn = document.querySelector('.container--image__nextbtn');
+prevbtn.addEventListener("click", () => {setSlidesImage(-1)});
+nextbtn.addEventListener("click", () => {setSlidesImage(1)});
+
+window.onload = () => {
+  changeSlide(slideIdx);
+  const setTime = 3000;
+  setInterval(() => {
+    slideIdx = slideIdx +1;
+    changeSlide(slideIdx);
+  }, setTime);
+}
+
+function setSlidesImage (num) {
+  slideIdx = slideIdx + num;
+  changeSlide(slideIdx);
+}
+
+
+function changeSlide (num) {
+  const slideList = document.getElementsByClassName('container--image__slides');
+  const slideLength = slideList.length;
+  console.log(slideLength);
+  if ((slideIdx +1) > slideLength) {
+    slideIdx = 0;
+    num = 0;
+    // 슬라이드의 마지막까지 갔다면 슬라이드 순서를 초기화
+    // num 값으로 슬라이드 순서가 정해지므로 해당 값도 0으로 설정.
+  } else if (num < 0) {
+    slideIdx = (slideLength -1);
+    num = (slideLength -1)
+    // prev 버튼을 계속 눌러 -1이 될 경우,
+    // 슬라이드의 가장 마지막 순서로 변경.
+  }
+  // 나머지 전부 display 없애고, 현재 슬라이드만 보이도록 함.
+
+  
+  for (let i = 0; i < slideLength; i++) {
+    slideList[i].style.display = "none";
+  }
+  slideList[num].style.display = "block";
+}
